ccc36f5cadeda81ab80865a5b5b9fcd1b6e5cd3e
lotuseana <143060902+lotuseana@users.noreply.github.com>
2024-04-22 10:12:46 -0700

    Update StepTracker.java

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -1,5 +1,24 @@
 import java.util.ArrayList;
 public class StepTracker
 {
- /* to be implemented here */
+ private int c, numDays, activeDays, totalSteps;
+ public StepTracker(){
+  c=a; 
+  numDays = 0; 
+  activeDays = 0;
+  totalSteps =0;
+ }
+ public void addDailySteps(int steps){
+  totalSteps+= steps;
+  numDays++;
+   if (steps>= c){
+   activeDays++;
+   }
+ }
+ public int activeDays(){
+  return activeDays;
+ }
+ public double averageSteps(){
+  return ((double)(totalSteps)/numDays);
+ }
 } 
